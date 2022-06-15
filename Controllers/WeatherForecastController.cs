db0dbb25acef0e1f086f65605358de6e2f7958e0
Caio Borges <caiocesarborges89@gmail.com>
2022-06-14 23:47:23 -0300

    feat: criado novos metodos na controller

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 using App.Metrics;
 using App.Metrics.Timer;
 using Microsoft.AspNetCore.Mvc;
+using Prometheus.Metrics.Demo.Model;
 
 namespace Prometheus.Metrics.Demo.Controllers;
 
@@ -24,7 +25,7 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public IEnumerable<WeatherForecastModel> Get()
     {
         var weatherForecastTimer = new TimerOptions
         {
@@ -35,15 +36,97 @@ public class WeatherForecastController : ControllerBase
         };
 
         using (_metrics.Measure.Timer.Time(weatherForecastTimer))
-
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecastModel
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+    }
 
-        
+    [HttpPost(Name = "SaveWeatherForecast")]
+    public IActionResult Post(WeatherForecastModel model)
+    {
+        var weatherForecastTimer = new TimerOptions
+        {
+            Name = "Save WeatherForecast",
+            MeasurementUnit = Unit.Commands,
+            DurationUnit = TimeUnit.Milliseconds,
+            RateUnit = TimeUnit.Milliseconds
+        };
+
+        using (_metrics.Measure.Timer.Time(weatherForecastTimer))
+        {
+            var result = new WeatherForecastModel
+            {
+                Date = DateTime.Now,
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                TemperatureC = Random.Shared.Next(-20, 55),
+            };
+
+            return Ok(result);
+        }
+    }
+
+    [HttpPut("/update")]
+    public IActionResult Put(int id, WeatherForecastModel model)
+    {
+        var weatherForecastTimer = new TimerOptions
+        {
+            Name = "Update WeatherForecast",
+            MeasurementUnit = Unit.Commands,
+            DurationUnit = TimeUnit.Milliseconds,
+            RateUnit = TimeUnit.Milliseconds
+        };
+
+        using (_metrics.Measure.Timer.Time(weatherForecastTimer))
+        {            
+            return NoContent();
+        }
+    }
+
+    [HttpPost("/post-error")]
+    public IActionResult PostError(WeatherForecastModel model)
+    {
+        var weatherForecastTimer = new TimerOptions
+        {
+            Name = "Save WeatherForecast Error",
+            MeasurementUnit = Unit.Commands,
+            DurationUnit = TimeUnit.Milliseconds,
+            RateUnit = TimeUnit.Milliseconds
+        };
+
+        using (_metrics.Measure.Timer.Time(weatherForecastTimer))
+        {
+            var result = Random.Shared.Next(1, 10);
+
+            if (result < 5)
+                return StatusCode(StatusCodes.Status400BadRequest, "Bad Request");
+            else
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
+        }
+    }
+
+    [HttpGet("/get-error")]
+    public IActionResult GetError()
+    {
+        var weatherForecastTimer = new TimerOptions
+        {
+            Name = "Get WeatherForecast Error",
+            MeasurementUnit = Unit.Commands,
+            DurationUnit = TimeUnit.Milliseconds,
+            RateUnit = TimeUnit.Milliseconds
+        };
+
+        using (_metrics.Measure.Timer.Time(weatherForecastTimer))
+        {
+            var result = Random.Shared.Next(1, 10);
+
+            if (result < 5)
+                return StatusCode(StatusCodes.Status400BadRequest, "Bad Request");
+            else
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
+        }
     }
 }
